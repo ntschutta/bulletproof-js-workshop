e27bff01664205bd14f2ecfd49f1e4e468e521e2
Nate Schutta <ntschutta@gmail.com>
2014-11-12 16:56:21 -0600

    shell gruntile

---
+++ EmptyGruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) { 
+  grunt.initConfig({ 
+    pkg: grunt.file.readJSON('package.json') ,
+
+    //templates will go here
+    }
+  });
+    //tasks will go here
+
+    //tasks can be registered here
+    grunt.registerTask('default', ['jshint']);
+};
